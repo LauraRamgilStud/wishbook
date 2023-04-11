75e7c073dc9cf390cc17e6fa47c7b3b3c9197501
Aileena <aile0074@stud.kea.dk>
2023-04-11 12:16:33 +0200

    setup

---
+++ src/main/java/com/wishbook/WishbookApplication.java
@@ -0,0 +1,13 @@
+package com.wishbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wishbook/WishbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
