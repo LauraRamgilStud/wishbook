77e9b4a4fc6e3a07f136bd6694ff550425ef9dfb
Aileena <aile0074@stud.kea.dk>
2023-04-19 15:34:25 +0200

    no

--- src/main/resources/static/javaScript/script.js
+++ src/main/resources/static/javaScript/script.js
@@ -1,14 +1,25 @@
-function toggleForm() {
-    var loginForm = document.getElementById("loginForm");
-    var loginButton = document.getElementById("loginButton");
-    if (loginForm.style.display === "block") {
-        loginForm.style.display = "none";
-        loginButton.classList.remove("hide");
-    } else {
-        loginForm.style.display = "block";
-        loginButton.classList.add("hide");
+    function toggleForm(isLogin) {
+        var loginForm = document.getElementById("loginForm");
+        var loginButton = document.getElementById("loginButton");
+        var cancelButton = document.getElementById("cancelButton");
+
+        if (isLogin) {
+            if (loginForm.style.display === "block") {
+                loginForm.style.display = "none";
+                loginButton.classList.remove("hide");
+            } else {
+                loginForm.style.display = "block";
+                loginButton.classList.add("hide");
+            }
+            cancelButton.style.display = "inline-block";
+            loginButton.style.display = "none";
+        } else {
+            loginForm.style.display = "none";
+            cancelButton.style.display = "none";
+            loginButton.style.display = "inline-block";
+        }
     }
-}
+
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("loginForm");
     form.style.display = "none";
